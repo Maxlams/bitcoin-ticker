2ba9e509a3dfb755f736417ec143efa683869e45
Max Lam <57158526+maxlambda@users.noreply.github.com>
2020-01-11 23:05:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// If you are using the Atom editor with
+// the linter-jshint package installed, add the
+// following comment:
+
 //jshint esversion:6
 
 const express = require("express");
@@ -6,6 +10,7 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
@@ -14,23 +19,24 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
 
-  // console.log(req.body.crypto);
-
-  var crypto = req.body.crypto;
-  var fiat = req.body.fiat;
+  var crypto = req.body.crypto; // from HTML
+  var fiat = req.body.fiat; // from HTML
   var bitcoinURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
-  var wholeURL = bitcoinURL + crypto + fiat;
+  var wholeURL = bitcoinURL + crypto + fiat; // final HTTP request link
 
   request(wholeURL, function(error, response, body) { // ("url", function(error, response, body))
-    var data = JSON.parse(body); // turns JSON (JavaScript Obejct Notation) file into JS file
-    var price = data.last; // key=last
-    var currDate = data.display_timestamp;
 
-    res.write("<p>The current date is " + currDate + "</p>");
+    var data = JSON.parse(body); // turns JSON (JavaScript Obejct Notation) file into JS file
+    var price = data.last; // key = last
+    var currDate = data.display_timestamp; // key = display_timestamp
+
+    res.write("<p>The current date and time is " + currDate + "</p>");
     res.write("<h1>The price of " + crypto + " is " + price + fiat + "</h1>");
 
     res.send();
+
   });
+
 });
 
 app.listen(3000, function() {
