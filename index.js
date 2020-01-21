694d2964e90baec6614edb4a179f07abf76236e8
Max Lam <57158526+maxlambda@users.noreply.github.com>
2020-01-20 23:49:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,32 +11,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 
+// create an instance of Express
 const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html"); // get from index.html
 });
-
-
 
 app.post("/", function(req, res) {
 
-  let crypto = req.body.crypto; // from HTML
-  let fiat = req.body.fiat; // from HTML
+  let crypto = req.body.crypto; // get cryptocurrency from HTML
+  let fiat = req.body.fiat; // get fiat currency from HTML
   let bitcoinURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
   let wholeURL = bitcoinURL + crypto + fiat; // final HTTP request link
 
-  request(wholeURL, function(error, response, body) { // ("url", function(error, response, body))
+  request(wholeURL, function(error, response, body) {
 
-    let data = JSON.parse(body); // turns JSON (JavaScript Obejct Notation) file into JS file
-    let price = data.last; // key = last
-    let currDate = data.display_timestamp; // key = display_timestamp
+    let data = JSON.parse(body);
+    let price = data.last; // fetch last price
+    let currDate = data.display_timestamp; // fetch timestamp
 
-    res.write("<p>" + currDate + "</p>");
-    res.write("<h1>The current price of " + crypto + " is " + price + fiat + "</h1>");
+    res.write("<p>The current date and time is " + currDate + ".</p>");
+    res.write("<h1>The price of " + crypto + " is currently " + price + fiat + ".</h1><h2>(this is a work in progress!)</h2>");
 
     res.send();
 
@@ -44,10 +43,6 @@ app.post("/", function(req, res) {
 
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server running on port 3000.");
 });
-
-
-
-
